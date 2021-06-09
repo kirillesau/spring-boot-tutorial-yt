ff5db20e6ed93b68bcbabf3e3324642d3a8d3553
Kirill Esau <E=$n9k19k3k4>
2021-06-09 20:25:21 +0200

    Init Commit

---
+++ src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottutorialyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialYtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTutorialYtApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springboottutorialyt/SpringBootTutorialYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottutorialyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialYtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
