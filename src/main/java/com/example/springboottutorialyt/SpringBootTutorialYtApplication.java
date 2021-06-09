8f0c0f9ef0ebecf8590dea5e10776827a7886704
Kirill Esau <E=$n9k19k3k4>
2021-06-09 20:34:32 +0200

    Hello World als RestController 16:30

--- src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
+++ src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
@@ -2,12 +2,19 @@ package com.example.springboottutorialyt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootTutorialYtApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootTutorialYtApplication.class, args);
   }
 
+  @GetMapping
+  public String hello() {
+    return "Hello World";
+  }
 }
