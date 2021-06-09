86885bf8160ff3783f033b9b0ec0bb7e519be0e7
Kirill Esau <E=$n9k19k3k4>
2021-06-09 20:45:51 +0200

    Erstellung eines Student-Models

--- src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
+++ src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
@@ -1,10 +1,13 @@
 package com.example.springboottutorialyt;
 
+import com.example.springboottutorialyt.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,9 @@ public class SpringBootTutorialYtApplication {
   }
 
   @GetMapping
-  public List<String> hello() {
-    return List.of("Hello", "World");
+  public List<Student> hello() {
+    return List.of(
+        new Student(1L, "Mustermann", "max.mustermann@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
+    );
   }
 }
