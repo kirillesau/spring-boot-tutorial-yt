2cc548a01402cd4207eb4894598c46f99eeb4918
Kirill Esau <E=$n9k19k3k4>
2021-06-10 15:53:58 +0200

    Auslagerung in einen StudentController

--- src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
+++ src/main/java/com/example/springboottutorialyt/SpringBootTutorialYtApplication.java
@@ -11,17 +11,10 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootTutorialYtApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootTutorialYtApplication.class, args);
   }
 
-  @GetMapping
-  public List<Student> hello() {
-    return List.of(
-        new Student(1L, "Mustermann", "max.mustermann@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
-    );
-  }
 }
